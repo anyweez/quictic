dbaec8aad1c8bd90b462c09a1d732a858644de00
Luke Segars <luke@lukesegars.com>
2013-01-11 15:34:57 -0800

    Arguments test

--- backend/parsedate.py
+++ backend/parsedate.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 
+import cgi
+
 def main():
 	print "Content-type: text/json"
 	print ""
 	print "{ 'response' : 5 }"
+	
+	arguments = cgi.FieldStorage()
+	for i in arguments.keys():
+		print arguments[i].value
 
 if __name__ == '__main__':
 	main()
