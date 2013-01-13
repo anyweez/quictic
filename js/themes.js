6929f94de3cc322d89694135e95232a26a784dc7
Luke Segars <luke@lukesegars.com>
2013-01-13 15:50:35 -0800

    Removed backend support from htaccess. Added theme support and dark + light themes.

--- js/themes.js
+++ js/themes.js
@@ -1,20 +1,44 @@
 
-function apply_theme(theme_id) {
-  // var themes = {
-//     'a' : {
-//       name : "Standard"
-//       func : standard
-//     }
-//   };
-  standard();
-//  themes[theme_id]();
+function get_themes() {
+  var themes = {
+    'a' : {
+      name : "Dark",
+      func : thm_standard,
+      preview: prv_standard
+    },
+    'b' : {
+      name : "Light",
+      func : thm_combo,
+      preview: prv_combo
+	}
+  };
+	
+  return themes;
 }
 
-function standard() {
+function apply_theme(theme_id) {
+  var themes = get_themes();
+  themes[theme_id].func();
+}
+
+/*******************
+ ** DEFAULT THEME **
+ *******************/
+
+/**
+ * Applies a representative style to the UI element that the represents
+ * this theme.
+ */
+function prv_standard(element) {
+  element.css('background-image', 'url(http://quictic.com/img/debut_dark.png)');
+}
+
+function thm_standard() {
   // Set global colors and fonts.
-  $('body').css('background-image', 'url(/img/debut_dark.png)');
+  $('body').css('background-image', 'url(http://quictic.com/img/debut_dark.png)');
   $('body').css('background-repeat', 'repeat');
-  $('body').css('color', '#fff');
+  $('#heading').css('color', '#DBDBAD');
+  $('#heading').css('text-shadow', '2px 2px 2px #333');
   
   // Title-specific properties.
   $('#title').css('font-size', '4em');
@@ -23,6 +47,7 @@ function standard() {
   $('#title').css('text-shadow', '2px 2px .1em #333');
 
   // Countdown-specific properties.
+  $('#countdown').css('color', '#fff');
   $('#countdown').css('font-size', '8em');
   $('#countdown').css('text-shadow', '2px 2px .1em #333');
   
@@ -41,3 +66,40 @@ function standard() {
   $('#body').css('background', 'rgba(255, 255, 255, .15)');
 }
 
+/*****************
+ ** COMBO THEME **
+ *****************/
+ 
+function prv_combo(element) {
+  element.css('background-image', 'url(http://localhost/quictic/img/nasty_fabric.png)');
+}
+
+function thm_combo() {
+  $('body').css('background-image', 'url(http://localhost/quictic/img/nasty_fabric.png)');
+  $('body').css('background-repeat', 'repeat');
+  $('h1').css('text-shadow', '0px 0px');
+  $('#heading').css('color', '#2A75A9');
+  $('#heading').css('text-shadow', 'none');
+  
+  // Title-specific properties.
+  $('#title').css('font-size', '4em');
+  $('#title').css('color', '#2A75A9');
+  $('#title').css('padding-bottom', '1%');
+  $('#title').css('text-shadow', '2px 2px .1em #fff');
+  
+  // Countdown-specific properties.
+  $('#countdown').css('color', '#222');
+  $('#countdown').css('font-size', '8em');
+  $('#countdown').css('text-shadow', '2px 2px .1em #fff');
+  
+  // Labels.
+  $('.label').css('font-size', '.3em');
+  $('#year_label').css('position', 'relative');
+  
+    // Links
+  $('a').css('text-decoration', 'none');
+  $('a').css('color', 'white');
+  
+  $('#body').css('background-color', 'rgba(255, 255, 255, .45)');
+	
+}
